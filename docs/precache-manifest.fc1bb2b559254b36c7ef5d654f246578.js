54b1969901959e7d1d6e1b4226c387fcf9f4b4e2
mmmveggies <jakeselig@gmail.com>
2020-06-14 21:27:19 -0600

    new files

---
+++ docs/precache-manifest.fc1bb2b559254b36c7ef5d654f246578.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1e24abece95a4f2e50f300423e2553",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc40a5715237fe5a0eed",
+    "url": "./static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "7ae07b54c5da0798a39d",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc40a5715237fe5a0eed",
+    "url": "./static/js/2.0c005519.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.0c005519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae07b54c5da0798a39d",
+    "url": "./static/js/main.e9177039.chunk.js"
+  },
+  {
+    "revision": "b70e7bebf0d460cb13f1",
+    "url": "./static/js/runtime-main.5c6b2873.js"
+  }
+]);
